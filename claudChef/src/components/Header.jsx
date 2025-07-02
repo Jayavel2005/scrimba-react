1e2c364b7b883ec78627af0d0847b2d4939e6b03
Jayavel <jayavel1520@gmail.com>
2025-07-02 20:45:41 +0530

    Ingredient form finished

--- claudChef/src/components/Header.jsx
+++ claudChef/src/components/Header.jsx
@@ -3,7 +3,7 @@ import claudeChefLogo from "../images/chef-claude-icon.png"
 const Header = () => {
   return (
     <header>
-      <nav className='flex justify-center items-center gap-3 shadow-md py-4'>
+      <nav className='flex justify-center items-center gap-4 shadow-md py-4 bg-white'>
         <img src={claudeChefLogo} alt="chef claude logo" className='w-[44px]' />
         <p className='text-3xl font-doto font-extrabold '>Chef Claude</p>
       </nav>
---
+++ claudChef/src/components/Main.jsx
@@ -0,0 +1,20 @@
+
+
+const Main = () => {
+    return (
+        <div className="p-4 mt-5">
+            <form className="flex-col flex sm:flex-row sm:justify-center  gap-5 ">
+                <div className="form  sm:w-lg w-full">
+                    <input
+                        type="text"
+                        name="Add an ingredient"
+                        placeholder="e.g. oregano"
+                        className=" rounded p-3 w-full caret outline-2 outline-black" />
+                </div>
+                <button className="bg-black px-5 max-sm:py-2 rounded-md text-white cursor-pointer hover:bg-gray-950">+ Add ingredient</button>
+            </form>
+        </div>
+    )
+}
+
+export default Main
