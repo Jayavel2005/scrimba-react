6e291fa7489e042b7df83ae0f982d470d458802d
Jayavel <jayavel1520@gmail.com>
2025-07-02 20:52:03 +0530

    Ingredient form finxed

--- claudChef/src/components/Main.jsx
+++ claudChef/src/components/Main.jsx
@@ -2,7 +2,7 @@
 
 const Main = () => {
     return (
-        <div className="p-4 mt-5">
+        <div className="p-5 my-10">
             <form className="flex-col flex sm:flex-row sm:justify-center  gap-5 ">
                 <div className="form  sm:w-lg w-full">
                     <input
@@ -11,7 +11,7 @@ const Main = () => {
                         placeholder="e.g. oregano"
                         className=" rounded p-3 w-full caret outline-2 outline-black" />
                 </div>
-                <button className="bg-black px-5 max-sm:py-2 rounded-md text-white cursor-pointer hover:bg-gray-950">+ Add ingredient</button>
+                <button className="bg-black px-5 max-sm:py-2 rounded-md text-white cursor-pointer hover:bg-gray-950 ">+ Add ingredient</button>
             </form>
         </div>
     )
